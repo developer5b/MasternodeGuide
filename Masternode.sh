10ce81a456a286dc3e618494dbbf2d67486b76e4
developer5b <blackmanjip@mail.ru>
2018-11-11 13:28:13 +0300

    fdgfd

--- Masternode.sh
+++ Masternode.sh
@@ -33,3 +33,64 @@ if [[ $DOSETUP =~ "y" ]] ; then
   source ~/.bashrc
 fi
 
+echo "Do you want to compile Daemon (please choose no if you did it before)? [y/n]"
+read DOSETUPTWO
+
+if [[ $DOSETUPTWO =~ "y" ]] ; then
+
+scio-cli stop > /dev/null 2>&1
+wget https://github.com/Lazzardev/SCIO/blob/master/wallet/linux64/sciod -O /usr/local/bin/sciod
+wget https://github.com/Lazzardev/SCIO/blob/master/wallet/linux64/scio-cli -O /usr/local/bin/scio-cli
+chmod +x /usr/local/bin/scio*
+
+fi
+
+echo ""
+echo "Configuring IP - Please Wait......."
+
+declare -a NODE_IPS
+for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
+do
+  NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+done
+
+if [ ${#NODE_IPS[@]} -gt 1 ]
+  then
+    echo -e "More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
+    INDEX=0
+    for ip in "${NODE_IPS[@]}"
+    do
+      echo ${INDEX} $ip
+      let INDEX=${INDEX}+1
+    done
+    read -e choose_ip
+    IP=${NODE_IPS[$choose_ip]}
+else
+  IP=${NODE_IPS[0]}
+fi
+
+echo "IP Done"
+echo ""
+echo "Enter masternode private key for node $ALIAS , Go To your Windows Wallet Tools > Debug Console , Type masternode genkey"
+read PRIVKEY
+
+CONF_DIR=~/.SCIO/
+CONF_FILE=scio.conf
+PORT=7770
+
+mkdir -p $CONF_DIR
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` > $CONF_DIR/$CONF_FILE
+echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
+echo "rpcport=7777" >> $CONF_DIR/$CONF_FILE
+echo "listen=1" >> $CONF_DIR/$CONF_FILE
+echo "server=1" >> $CONF_DIR/$CONF_FILE
+echo "daemon=1" >> $CONF_DIR/$CONF_FILE
+echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
+echo "masternode=1" >> $CONF_DIR/$CONF_FILE
+echo "port=$PORT" >> $CONF_DIR/$CONF_FILE
+echo "masternodeaddr=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
+echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
+
+sciod -daemon
+
